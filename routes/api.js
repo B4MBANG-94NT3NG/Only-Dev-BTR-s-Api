6ae6aad715139f36e09892a5db774bf1d6c3d1ab
B4MB4N9-ID <68556996+B4MBANG-94NT3NG@users.noreply.github.com>
2021-03-27 17:30:35 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -180,7 +180,7 @@ router.get('/find', async (req, res, next) => {
     }
 })
 
-router.get('/cekapikey', async (req, res, next) => {
+router.get('cekapikey', async (req, res, next) => {
 	var apikeyInput = req.query.apikey
 	if(!apikeyInput) return res.json(loghandler.notparam)
 	a = await cekApiKey(apikeyInput)
