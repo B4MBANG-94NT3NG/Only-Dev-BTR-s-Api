836cfca592369f1d989e54cc69e7732ee27db181
B4MB4N9-ID <68556996+B4MBANG-94NT3NG@users.noreply.github.com>
2021-03-28 02:06:24 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -166,7 +166,7 @@ router.get('/web/cekapikey', async (req, res, next) => {
     if (apikey != 'BambangGans') return res.json(loghandler.invalidKey)
 
     try {
-        BambangGans.web/cekapikey()
+        BambangGans.web()
             .then(result => {
                 res.json({
                     status: true,
